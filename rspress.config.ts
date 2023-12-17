24da0c6ef5684e6cc1d28905d3bce34d8194e709
yunumu <1416900346@qq.com>
2023-12-17 17:36:10 +0800

    修改配置

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,15 +2,16 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-	base: process.env.NODE_ENV === 'development' ? '' : '/fe-rs-blog',
+	base: process.env.NODE_ENV === 'development' ? '' : '/fe-rs-blog/',
 	root: path.join(__dirname, 'docs'),
 	title: 'fe-blog',
-	description: 'Rspack-based Static Site Generator',
+	description: 'fe-rspress-blog',
 	icon: '/rspress-icon.png',
 	logo: {
 		light: '/rspress-light-logo.png',
 		dark: '/rspress-dark-logo.png',
 	},
+	ssg: false,
 	themeConfig: {
 		socialLinks: [
 			{
@@ -19,5 +20,6 @@ export default defineConfig({
 				content: 'https://github.com/web-infra-dev/rspress',
 			},
 		],
+		hideNavbar: 'never',
 	},
 });
