794ec15ea4abff313dc323d2ae7cb43af1522176
yunumu <1416900346@qq.com>
2023-12-17 16:36:15 +0800

    增加 base 配置

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
+	base: '/fe-rs-blog'
 	root: path.join(__dirname, 'docs'),
 	title: 'fe-blog',
 	description: 'Rspack-based Static Site Generator',
