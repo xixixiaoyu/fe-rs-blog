58b743a08b76385ddd8396fc2f6f680b35c379fc
云牧 <yunmucoder@163.com>
2024-10-22 17:20:58 +0800

    docs: 添加 Katex 支持并优化代码样式
    - 在 global.css 中引入 Katex 的 CSS 文件
    - 更新 rspress.config.ts 以支持 Katex 插件
    - 优化代码格式和可读性

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,71 +1,71 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
-import rehypeSlug from 'rehype-slug';
-import rehypeRaw from 'rehype-raw';
-import remarkAutolinkHeadings from 'remark-autolink-headings';
-import rehypeKatex from 'rehype-katex';
+import * as path from 'path'
+import { defineConfig } from 'rspress/config'
+import rehypeSlug from 'rehype-slug'
+import rehypeRaw from 'rehype-raw'
+import remarkAutolinkHeadings from 'remark-autolink-headings'
+import rehypeKatex from 'rehype-katex'
 
 export default defineConfig({
-	base: process.env.NODE_ENV === 'development' ? '' : '/fe-rs-blog/',
-	root: path.join(__dirname, 'docs'),
-	title: 'fe-blog',
-	description: 'fe-rspress-blog',
-	icon: '/favicon.ico',
-	logo: '/favicon.ico',
-	themeConfig: {
-		socialLinks: [
-			{
-				icon: 'juejin',
-				mode: 'link',
-				content: 'https://juejin.cn/user/1530130204207822',
-			},
-			{
-				icon: 'bilibili',
-				mode: 'link',
-				content: 'https://space.bilibili.com/145679856',
-			},
-			{
-				icon: 'github',
-				mode: 'link',
-				content: 'https://github.com/xixixiaoyu',
-			},
-		],
-		hideNavbar: 'never',
-		outlineTitle: '目录',
-	},
-	markdown: {
-		mdxRs: false,
-		remarkPlugins: [
-			[
-				remarkAutolinkHeadings,
-				{
-					behavior: 'wrap',
-				},
-			],
-		],
-		rehypePlugins: [rehypeSlug, rehypeRaw, rehypeKatex as any],
-	},
-	globalStyles: path.join(__dirname, '/docs/styles/global.css'),
-	builderConfig: {
-		html: {
-			tags: [
-				{
-					tag: 'meta',
-					attrs: {
-						name: 'referrer',
-						content: 'no-referrer',
-					},
-				},
-			],
-		},
-		tools: {
-			rspack: config => {
-				config.module.rules.push({
-					test: /\.(woff|woff2|eot|ttf|otf)$/i,
-					type: 'asset/resource',
-				});
-				return config;
-			},
-		},
-	},
-});
+  base: process.env.NODE_ENV === 'development' ? '' : '/fe-rs-blog/',
+  root: path.join(__dirname, 'docs'),
+  title: 'fe-blog',
+  description: 'fe-rspress-blog',
+  icon: '/favicon.ico',
+  logo: '/favicon.ico',
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'juejin',
+        mode: 'link',
+        content: 'https://juejin.cn/user/1530130204207822',
+      },
+      {
+        icon: 'bilibili',
+        mode: 'link',
+        content: 'https://space.bilibili.com/145679856',
+      },
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/xixixiaoyu',
+      },
+    ],
+    hideNavbar: 'never',
+    outlineTitle: '目录',
+  },
+  markdown: {
+    mdxRs: false,
+    remarkPlugins: [
+      [
+        remarkAutolinkHeadings,
+        {
+          behavior: 'wrap',
+        },
+      ],
+    ],
+    rehypePlugins: [rehypeSlug, rehypeRaw, rehypeKatex],
+  },
+  globalStyles: path.join(__dirname, '/docs/styles/global.css'),
+  builderConfig: {
+    html: {
+      tags: [
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'referrer',
+            content: 'no-referrer',
+          },
+        },
+      ],
+    },
+    tools: {
+      rspack: config => {
+        config.module.rules.push({
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        })
+        return config
+      },
+    },
+  },
+})
