8c46f7f4ea89eff039352d98cd0d2089bfb38e3a
云牧 <yunmucoder@163.com>
2024-10-29 19:50:42 +0800

    docs(psychology): 添加土地革命战争时期的基本经验文章
    - 新增文章总结了土地革命战争时期（1927-1937年）中国共产党的宝贵经验
    - 文章涵盖了武装斗争、土地革命、建立根据地、统一战线、党的领导和灵活的战略战术等方面
    - 这些经验为后来的革命胜利奠定了坚实的基础，对中国革命具有重要指导意义

--- generateMeta.js
+++ generateMeta.js
@@ -1,31 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const whitelist = ['_meta.json', 'generateMeta.js', '.DS_Store'];
+const whitelist = ['_meta.json', 'generateMeta.js', '.DS_Store']
 
 // 获取当前工作目录
-const currentDir = process.cwd();
+const currentDir = process.cwd()
 
 try {
-	// 同步读取目录
-	const files = fs.readdirSync(currentDir).filter(item => !whitelist.includes(item));
+  // 同步读取目录
+  const files = fs.readdirSync(currentDir).filter(item => !whitelist.includes(item))
 
-	// 过滤并映射文件到所需的对象结构
-	const result = files
-		.filter(file => fs.statSync(path.join(currentDir, file)).isFile())
-		.map(file => ({
-			type: 'file',
-			name: path.parse(file).name,
-			label: path.parse(file).name,
-		}));
+  // 过滤并映射文件到所需的对象结构
+  const result = files
+    .filter(file => fs.statSync(path.join(currentDir, file)).isFile())
+    .map(file => ({
+      type: 'file',
+      name: path.parse(file).name,
+      label: path.parse(file).name,
+    }))
 
-	// 同步写入或覆盖 _meta.json 文件
-	fs.writeFileSync(
-		path.join(currentDir, '_meta.json'),
-		JSON.stringify(result, null, '\t')
-	);
+  // 同步写入或覆盖 _meta.json 文件
+  fs.writeFileSync(path.join(currentDir, '_meta.json'), JSON.stringify(result, null, '\t'))
 
-	console.log('_meta.json 已成功写入或覆盖到当前工作目录。');
+  console.log('_meta.json 已成功写入或覆盖到当前工作目录。')
 } catch (err) {
-	console.error('发生错误:', err);
+  console.error('发生错误:', err)
 }
