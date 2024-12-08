1cb9709b1b5d07af810e5f807cb199479f71628f
yunumu <1416900346@qq.com>
2024-12-08 19:03:36 +0800

    优化 generateMeta.js 文件的代码格式，调整缩进和注释，使代码更易读

--- generateMeta.js
+++ generateMeta.js
@@ -7,22 +7,25 @@ const whitelist = ['_meta.json', 'generateMeta.js', '.DS_Store']
 const currentDir = process.cwd()
 
 try {
-  // 同步读取目录
-  const files = fs.readdirSync(currentDir).filter(item => !whitelist.includes(item))
+	// 同步读取目录
+	const files = fs.readdirSync(currentDir).filter(item => !whitelist.includes(item))
 
-  // 过滤并映射文件到所需的对象结构
-  const result = files
-    .filter(file => fs.statSync(path.join(currentDir, file)).isFile())
-    .map(file => ({
-      type: 'file',
-      name: path.parse(file).name,
-      label: path.parse(file).name,
-    }))
+	// 过滤并映射文件到所需的对象结构
+	const result = files
+		.filter(file => fs.statSync(path.join(currentDir, file)).isFile())
+		.map(file => ({
+			type: 'file',
+			name: path.parse(file).name,
+			label: path.parse(file).name,
+		}))
 
-  // 同步写入或覆盖 _meta.json 文件
-  fs.writeFileSync(path.join(currentDir, '_meta.json'), JSON.stringify(result, null, '\t'))
+	// 同步写入或覆盖 _meta.json 文件
+	fs.writeFileSync(
+		path.join(currentDir, '_meta.json'),
+		JSON.stringify(result, null, '\t')
+	)
 
-  console.log('_meta.json 已成功写入或覆盖到当前工作目录。')
+	console.log('_meta.json 已成功写入或覆盖到当前工作目录。')
 } catch (err) {
-  console.error('发生错误:', err)
+	console.error('发生错误:', err)
 }
